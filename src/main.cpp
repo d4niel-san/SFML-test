f57b4350e5a12954a7d2c051d68db2c247eb99ff
d4aniel-San <dmermoz@gmail.com>
2024-05-28 17:56:08 -0300

    inicio de ejercicio 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "sfml-utn-inspt.h"
+#include <iostream>
+
 
 const unsigned int ANCHO_VENT = 800;
 const unsigned int ALTO_VENT = 600;
@@ -11,11 +13,25 @@ int main()
   float ratio = 75;
   int sides = 3;
   bool up = true;
+  int starParameter = 20;
 
   RenderWindow window = RenderWindow{{ANCHO_VENT, ALTO_VENT}, "Unidad 1 - Taller de Videojuegos"};
   window.setFramerateLimit(FRAMERATE);
 
-  CircleShape poligon;
+  CircleShape poligon, aux1, aux2, star;
+
+  aux1 = CircleShape(starParameter*2,5);
+    aux2 = CircleShape(starParameter,5);
+    aux2.setFillColor(Color::Red);
+    aux2.rotate(180);
+    aux2.setPosition(starParameter*3,starParameter*3);
+    
+    for (unsigned int i = 0; i < aux1.getPointCount(); ++i) {
+        sf::Vector2f point = aux1.getPoint(i);
+        std::cout << "Point " << i << " pentagono exterior: (" << point.x << ", " << point.y << ")\n";
+        point = aux2.getPoint(i);
+        std::cout << "Point " << i << " pentagono interior: (" << point.x << ", " << point.y << ")\n";
+    }
 
   while (window.isOpen())
   {
@@ -25,13 +41,30 @@ int main()
     (sides > 3 && !up) && sides--;
     if (sides==3) up=true;
 
-
     poligon = CircleShape(ratio, sides);
     poligon.setPosition({ANCHO_VENT / 2 - ratio, ALTO_VENT / 2 - ratio});
+
+    /* aux1 = CircleShape(starParameter*2,5);
+    aux2 = CircleShape(starParameter,5);
+    aux2.setFillColor(Color::Red);
+    aux2.rotate(180);
+    aux2.setPosition(starParameter*3,starParameter*3);
+    
+    for (unsigned int i = 0; i < aux1.getPointCount(); ++i) {
+        sf::Vector2f point = aux1.getPoint(i);
+        std::cout << "Point " << i << ": (" << point.x << ", " << point.y << ")\n";
+    }
+
+    for (unsigned int i = 0; i < aux2.getPointCount(); ++i) {
+        sf::Vector2f point = aux1.getPoint(i);
+        std::cout << "Point " << i << ": (" << point.x << ", " << point.y << ")\n";
+    } */
 
     read_events(window);
     window.clear();
     window.draw(poligon);
+    window.draw(aux1);
+    window.draw(aux2);
     window.display();
   }
 }
